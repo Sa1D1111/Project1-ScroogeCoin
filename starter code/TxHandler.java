6aae64adab022721c37fa19ce4368992891a4fe2
amandashohdy <amandashohdy@gmail.com>
2025-02-21 18:28:21 -0800

    isValidTx() 7/7 Tests Passed

--- starter code/TxHandler.java
+++ starter code/TxHandler.java
@@ -1,43 +1,66 @@
-public class TxHandler {
+import java.util.HashSet ; 
+import java.util.Set ; 
+import java.util.ArrayList ; 
 
+public class TxHandler {
     /* Creates a public ledger whose current UTXOPool (collection of unspent 
      * transaction outputs) is utxoPool. This should make a defensive copy of 
      * utxoPool by using the UTXOPool(UTXOPool uPool) constructor.
      */
+    private UTXOPool utxoPool ; // pool of unspent transactions
+
     public TxHandler(UTXOPool utxoPool) {
-        // IMPLEMENT THIS
+        this.utxoPool = new UTXOPool(utxoPool) ; // initializes transaction handler with copy of given UTXO pool
     }
 
-    /* Returns true if 
-     * (1) all outputs claimed by tx are in the current UTXO pool, 
-     * (2) the signatures on each input of tx are valid, 
-     * (3) no UTXO is claimed multiple times by tx, 
-     * (4) all of tx’s output values are non-negative, and
-     * (5) the sum of tx’s input values is greater than or equal to the sum of   
-            its output values;
-       and false otherwise.
-     */
+    /**********************************************************************************
+     * public boolean isValidTx(Transaction tx)
+     *      Returns true if 
+     *      (1) all outputs claimed by tx are in the current UTXO pool, 
+     *      (2) the signatures on each input of tx are valid, 
+     *      (3) no UTXO is claimed multiple times by tx, 
+     *      (4) all of tx’s output values are non-negative, and
+     *      (5) the sum of tx’s input values is greater than or equal to the sum of   
+                its output values;
+            and false otherwise.
+     **********************************************************************************/
+    public boolean isValidTx(Transaction tx) 
+    {
+        Set<UTXO> usedUTXOs = new HashSet<>() ; // store used UTXOs to detect double spending
+        ArrayList<Transaction.Input> inputs = tx.getInputs() ; 
+        ArrayList<Transaction.Output> outputs = tx.getOutputs() ;
+        double inputSum = 0 ;
+        double outputSum = 0 ;  
 
-    public boolean isValidTx(Transaction tx) {
+        for (int i = 0 ; i < tx.numInputs() ; i++) 
+        {
+            Transaction.Input input = inputs.get(i) ; 
+            UTXO utxo = new UTXO(input.prevTxHash, input.outputIndex) ; // UTXO based on prev transaction hash and output index
+            Transaction.Output output = utxoPool.getTxOutput(utxo) ; // retreive corresponding output from pool
 
         /* (1) ALL OUTPUTS CLAIMED BY TX ARE IN THE CURRENT UTXO POOL */
+        if (!utxoPool.contains(utxo)) { return false ; }
         
         /* (2) SIGNATURES ON EACH INPUT OF TX ARE VALID */
-        for (int i = 0 ; i < tx.numInputs() ; i++) { if (!rsaKey.verifySignature(tx.getRawDataToSign(i), tx.getInput(i).signature)) { return false ; } } ;
+        RSAKey rsa = output.address ; 
+        if (!rsa.verifySignature(tx.getRawDataToSign(i), tx.getInput(i).signature)) { return false ; } // attempt to verify signature
 
         /* (3) NO UTXO IS CLAIMED MULTIPLE TIMES BY TX */
+        if (!usedUTXOs.add(utxo)) { return false ; } // 
 
+        inputSum += output.value ; // add input value to sum variable
+        }
+
+        for (Transaction.Output output : outputs) 
+        { 
         /* (4) ALL OF TX'S OUTPUT VALUES ARE NON-NEGATIVE */
-        for (Transaction.Output output : tx.getOutputs()) { if (output.value < 0) { return false ; } }
-
+            if (output.value < 0) { return false ; } 
+            
         /* (5) SUM OF TX'S INPUT VALUES IS GREATER THAN OR EQUAL TO THE SUM OF ITS OUTPUT VALUES */
-        double inputSum = 0 ;
-        for (Transaction.Input input : tx.getInputs()) { /* inputSum += input. USE THE OUTPUT VALUE FROM THE PREVIOUS TRANSACTION BEING USED */; }
-        
-        double outputSum = 0 ;
-        for (Transaction.Output output : tx.getOutputs()) { outputSum += output.value ; }
+            outputSum += output.value ; 
+        }
 
-        if (inputSum >= outputSum) { return true ; }
+        if (!(inputSum >= outputSum)) { return false ; }
 
         return true ;
     }
